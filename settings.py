4ebb079190341a35c3ba4c50b11559f7bce1630f
Katia <63531419+findingxvlasova@users.noreply.github.com>
2021-07-19 08:26:24 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,13 @@
+class Settings():
+     def __init__(self):
+         self.screenWidth = 1000
+         self.screenHeight = 650
+         self.bgColor = (230, 230, 230)
+
+         self.shipSpeedFactor = 1.5
+
+         self.bulletSpeedFactor = 1
+         self.bulletWidth = 3 #pix
+         self.bulletHeight = 15
+         self.bulletColor = 60, 60, 60
+         self.bulletsAllowed = 3
